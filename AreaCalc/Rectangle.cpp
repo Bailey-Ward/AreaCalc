9cd3e913241fa920425a66a251923d6da1b4bdef
Bailey Ward <25754670@students.lincoln.ac.uk>
2022-11-02 10:03:46 +0000

    finished

--- AreaCalc/Rectangle.cpp
+++ AreaCalc/Rectangle.cpp
@@ -7,5 +7,5 @@ Rectangle::Rectangle(int width, int height) : Shape(width, height)
 
 void Rectangle::Area() {
 	int areaValue = getWidth() * getHeight();
-	std::cout << "Width: " << getWidth() << " * " << "Height: " << getHeight() << " = " << "Area: " << areaValue << "\t";
+	std::cout << "Rectangle:\nWidth: " << getWidth() << " * " << "Height: " << getHeight() << " = " << "Area: " << areaValue << "\t";
 }
--- AreaCalc/Triangle.cpp
+++ AreaCalc/Triangle.cpp
@@ -7,5 +7,5 @@ Triangle::Triangle(int width, int height) : Shape(width, height)
 
 void Triangle::Area() {
 	int areaValue = getWidth() * getHeight() / 2;
-	std::cout << areaValue << "\t";
+	std::cout << "\nTriangle:\n" << "Width:" << getWidth() << " * " << "Height:" << getHeight() << " / 2 " << " = " << areaValue << "\t";
 }
